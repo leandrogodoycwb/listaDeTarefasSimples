aa91f84652c88ad7871cb5130ae9f4d40bd8bb83
LEANDRO\Leandro L. Godoy <leandrolenon@gmail.com>
2022-07-27 00:34:37 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/todo_list_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: TodoListPage(),
+    );
+  }
+}
